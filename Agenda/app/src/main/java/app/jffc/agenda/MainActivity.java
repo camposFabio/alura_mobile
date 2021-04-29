a5e86cb9128de48d1ec1af5e34272f3af9a40e2a
fabio <campos_fabio@hotmail.com>
2021-04-29 14:28:53 -0300

    01 - Cria a sua primeira app mobile

---
+++ Agenda/app/src/main/java/app/jffc/agenda/MainActivity.java
@@ -0,0 +1,19 @@
+package app.jffc.agenda;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        Toast.makeText(this, "Fabio Campos", Toast.LENGTH_LONG).show();
+
+        setContentView(R.layout.activity_main);
+    }
+}
