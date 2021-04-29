14cbca3b9536e5733c9569e663beff41fabb0cc5
fabio <campos_fabio@hotmail.com>
2021-04-29 15:21:21 -0300

    02 - Aplicando Layouts para Activities

--- Agenda/app/src/main/java/app/jffc/agenda/MainActivity.java
+++ Agenda/app/src/main/java/app/jffc/agenda/MainActivity.java
@@ -2,18 +2,34 @@ package app.jffc.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//        Toast.makeText(this, "Fabio Campos", Toast.LENGTH_LONG).show();
-
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(Arrays.asList("Fred", "Juno", "Fabio", "Lilia"));
+
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos));
+
     }
 }
